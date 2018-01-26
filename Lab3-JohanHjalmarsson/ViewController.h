89042fb19c1a29edfba8f1c547badb56857b6db7
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2018-01-26 10:48:58 +0100

    Initial Commit

---
+++ Lab3-JohanHjalmarsson/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab3-JohanHjalmarsson
+//
+//  Created by Johan Hjalmarsson on 2018-01-26.
+//  Copyright © 2018 Johan Hjalmarsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lab3-JohanHjalmarsson/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Lab3-JohanHjalmarsson
+//
+//  Created by Johan Hjalmarsson on 2018-01-26.
+//  Copyright © 2018 Johan Hjalmarsson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
